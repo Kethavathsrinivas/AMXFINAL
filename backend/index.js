59639ed8c8a592efc0eaf97b3e4a0668941ff0b3
srinivas <srinivas@example.com>
2024-08-31 10:44:30 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000; // Use the port you prefer
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin:[""],
+  origin:["https://amxfinal.vercel.app/"],
   methods : ["POST","GET"],
   credentials:true,
 }));
